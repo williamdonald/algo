c75a4b49fe41b1d058dc9e954681d7ab0eb11faf
williamdonald <guoiii@hotmail.com>
2011-10-29 17:29:40 +0800

    some bitwise-based applicatons

--- math/math.py
+++ math/math.py
@@ -33,3 +33,84 @@ def powerof2(x):
 #bitwise 
 def isKbitSet(x,k):
   return (x & 1<<(k-1)) != 0
+
+#lowest bit set off
+#for any integer x, it diffs with x-1 on lowest bit 
+def lowestoff(x):
+  return x & (x-1)
+
+#extract,based on
+#1 ^ 0 = 1
+#0 ^ 0 = 0
+def extractLowestBit(x):
+  return x ^ (x &(x-1))
+
+#toggle, based on
+#0 ^ 1 = 1 (0->1)
+#1 ^ 1 = 0 (1->0)
+def toggleKbitInX(x,k):
+  return x ^ 1<<(k-1)
+
+#force, based on
+#0 | 1 = 1
+#1 | 1 = 1
+def forceKbitSet(x,k):
+  return x | 1<<(k-1)
+
+#for 32 bit integer
+#1000 0000 0000 0000 0000 0000 0000 0000
+#1100 0000 0000 0000 0000 0000 0000 0000
+#1111 0000 0000 0000 0000 0000 0000 0000
+#1111 1111 0000 0000 0000 0000 0000 0000
+#1111 1111 1111 1111 0000 0000 0000 0000
+#1111 1111 1111 1111 1111 1111 1111 1111
+def setMostSignificantBitOnly(x):
+  y = x
+  y = y | (y>>1)
+  y = y | (y>>2)
+  y = y | (y>>4)
+  y = y | (y>>8)
+  y = y | (y>>16)
+  y = (y+1)>>1
+  return y
+
+################################
+#The captain of the ship TITANIC is a little .... off the track.
+#He needs to select the crew for the ship.
+#But everyone seems to be eligible.
+#So to test their intelligence, he plays a game. 
+#The contestants have to stand in a line.
+#They are given the numbers in the order in which they stand, starting from 1.
+#The captain then removes all the contestants that are standing at an odd position. 
+#Initially, standing people have numbers  1,2,3,4,5...
+#After first pass, people left are  2,4,...
+#After second pass  4,....
+#And so on.
+#You want to board the ship as a crew member.
+#Given the total number of applicants for a position, find the best place to stand in the line so that you are selected.
+################################
+
+
+#   4321 round
+#   |||| flag=1 killed in this round
+#   ||||
+# 8 1000
+# 7 0111
+# 6 0110
+# 5 0101
+# 4 0100
+# 3 0011
+# 2 0010
+# 1 0001
+
+
+# or based on math induction: return 2 ** math.floor(math.log(x,2))
+def jTitanic(x):
+  return setMostSignificantBitOnly(x)
+
+
+
+
+
+
+
